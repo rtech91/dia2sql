43f35bac13450247a776c0a1b869e4cc58073fd4
Andrew [RTech] <in0mad91@gmail.com>
2021-02-19 14:05:24 +0200

    Re-work command line arguments parsing with argparse.ArgumentParser

--- dia2sql.py
+++ dia2sql.py
@@ -1,52 +1,24 @@
 #!/usr/bin/python3
 
 from sys import exit, argv
-from getopt import getopt, GetoptError
 from diaparser.parser import DiaParser
 from app.settings import AppSettings
-
-def show_help():
-    """Show app usage help information"""
-
-    print ("""
-        \rOVERVIEW: Convert Dia RDBMS model to the specified SQL engine schema.\n
-        \rUSAGE: dia2sql --from-dia <*.dia file> --to-sql <*.sql file> --db-system <type>\n\n
-        \rOPTIONS:
-        \r  -h, --help\t\t Show help information.
-        \r  -f, --from-dia\t Path to Dia file with RDBMS model.
-        \r  -t, --to-sql\t\t Path to SQL file to be saved.
-        \r  -d, --db-system\t Preferred database management system.
-    """)
+from argparse import ArgumentParser
 
 def main(argv):
 
-    try:
-        # try to parse arguments from command line
-        opts, args = getopt(argv, 'hf:t:d:', ['help', 'from-dia=', 'to-sql=', 'db-system='])
-    except GetoptError as err:
-        print(err)
-        print("\nUse dia2sql --help to view usage help.")
-        exit(2)
+    parser = ArgumentParser()
+    parser.description = "Convert Dia RDBMS model to the specified SQL engine schema."
+    parser.add_argument('-f', '--from-dia', metavar="FILE", required=True, help="Path to Dia file with RDBMS model.")
+    parser.add_argument('-t', '--to-sql', metavar="FILE", required=True, help="Path to SQL file to be saved.")
+    parser.add_argument('-d', '--db-system', metavar="RDBMS type", required=False, default='mysql', help="Preferred database management system.")
+    args = parser.parse_args();
 
     settings: AppSettings = AppSettings()
-    for opt, arg in opts:
-        if opt in ('-h', '--help'):
-            show_help()
-            exit()
-        elif opt in ("-f", "--from-dia"):
-            settings.dia_path = arg
-        elif opt in ("-t", "--to-sql"):
-            settings.sql_path = arg
-        elif opt in ("-d", "--db-system"):
-            settings.sql_type = arg
+    settings.dia_path = args.from_dia
+    settings.sql_path = args.to_sql
+    settings.sql_type = args.db_system
     
-    del opts, args
-    
-    # if some arguments are empty, forcely show help information
-    if settings.dia_path == '' or settings.sql_path == '' or settings.sql_type == '':
-        show_help()
-        exit()
-
     dia = DiaParser(settings.dia_path)
     dia.saveSQL(settings.sql_path)
 
